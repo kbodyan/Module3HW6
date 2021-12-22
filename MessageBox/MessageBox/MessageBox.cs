8873d345176ac25ef884a8387364fa981ea1e62b
kbodyan <kbodyan@gmail.com>
2021-12-22 17:14:49 +0200

    done

---
+++ MessageBox/MessageBox/MessageBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageBox
+{
+    public class MessageBox
+    {
+        public event Action<State> WindowsClosedHandle;
+        public async void Open()
+        {
+            Console.WriteLine("Window opened");
+            await Task.Delay(3000);
+            Console.WriteLine("Window closed");
+            var random = new Random();
+            var state = random.Next(0, 1) == 0 ? State.Ok : State.Cancel;
+            WindowsClosedHandle?.Invoke(state);
+        }
+    }
+}
---
+++ MessageBox/MessageBox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MessageBox;
+
+namespace MessageBox
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var obj = new MessageBox();
+            var tcs = new TaskCompletionSource();
+            obj.WindowsClosedHandle += (state) =>
+            {
+                Console.WriteLine(state == State.Ok ? "Opened" : "Closed");
+                tcs.SetResult();
+            };
+            obj.Open();
+            tcs.Task.GetAwaiter().GetResult();
+        }
+    }
+}
